e507917ff3831d870cd23656edf733face14f273
Noah Ward <noahtward08@gmail.com>
2026-01-22 14:04:32 -0600

    Revises inputs to only allow valid moves + exception handling

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         while (true) { 
             gameBoard.displayBoard();
             //player turn
-            gameBoard.makeMove(makePlayerMove(scanner), Moves.X); //player move
+            gameBoard.makeMove(makePlayerMove(gameBoard.getValidMoves(), scanner), Moves.X); //player move
             //TODO: check win
 
 
@@ -21,9 +21,32 @@ public class Main {
         }
     }
 
-    static int makePlayerMove(Scanner scanner) {
-        System.out.println("Enter move (1-9): ");
-        return scanner.nextInt() - 1;
+    static int makePlayerMove(ArrayList<Integer> validMoves, Scanner scanner) {
+        int playerMove;
+        
+        while (true) { // gets user input
+            System.out.println(validMoves);
+            System.out.print("Enter move (1-9): ");
+
+            try {
+                playerMove = scanner.nextInt();
+
+                if (playerMove >= 1 && playerMove <= 9) { //if valid input
+                    if (validMoves.contains(playerMove - 1)) { //if input not a valid move
+                        break;
+                    } else {
+                        System.out.println("Invalid input - Space already taken.");
+                    }
+                } else {
+                    System.out.println("Invalid input - Number must be between 1 and 9.");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input - Try again.");
+                scanner.nextLine();
+            }
+        }
+
+        return playerMove - 1;
     }
 
     static int makeComputerMove(ArrayList<Integer> validMoves) { //for now just a random computer move
