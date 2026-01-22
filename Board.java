8416bbcb70cabda0c850539d79d5fc9f5d27f743
Noah Ward <noahtward08@gmail.com>
2026-01-22 13:15:12 -0600

    Adds basic game loop, no win check yet, and random comp moves

--- Board.java
+++ Board.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public class Board {
     private final Moves[] board;
     private final int BOARD_SIZE = 9;
@@ -10,13 +13,34 @@ public class Board {
     }
 
     public void displayBoard() { //nicely displays the board as a 2d grid 3x3
+        Moves squareValue;
         System.out.println("+---+---+---+");
         for (int i = 0; i < BOARD_SIZE; ++i) {
-            System.out.print("| " + board[i].getValue() + " ");
+            squareValue = board[i];
+            if (squareValue != Moves.EMPTY) { //if empty, put index number in the square, otherwise write X or O
+                System.out.print("| " + squareValue.getValue() + " ");
+            } else {
+                System.out.print("| " + (i + 1) + " ");
+            }
+            
             if (i % 3 == 2) { //for every third value go to new line, so we display rows of 3
                 System.out.println("|");
                 System.out.println("+---+---+---+");
             }
         }
+    }
+
+    public void makeMove(int index, Moves move) {
+        board[index] = move;
+    }
+
+    public ArrayList<Integer> getValidMoves() {
+        ArrayList<Integer> validMoves = new ArrayList<>();
+        for (int i = 0; i < BOARD_SIZE; ++i) {
+            if (board[i] == Moves.EMPTY) {
+                validMoves.add(i);
+            }
+        }
+        return validMoves;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Board gameBoard = new Board();
+        MinimaxAi computer = new MinimaxAi();
 
-        gameBoard.displayBoard();
+        while (true) { 
+            gameBoard.displayBoard();
+            //player turn
+            gameBoard.makeMove(makePlayerMove(scanner), Moves.X); //player move
+            //TODO: check win
+
+
+            //computer turn
+            gameBoard.displayBoard();
+            gameBoard.makeMove(makeComputerMove(gameBoard.getValidMoves()), Moves.O);
+        }
+    }
+
+    static int makePlayerMove(Scanner scanner) {
+        System.out.println("Enter move (1-9): ");
+        return scanner.nextInt() - 1;
+    }
+
+    static int makeComputerMove(ArrayList<Integer> validMoves) { //for now just a random computer move
+        Random rng = new Random();
+
+        return validMoves.get(rng.nextInt(validMoves.size()));
     }
 }
